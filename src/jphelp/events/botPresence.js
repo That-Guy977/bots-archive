1b6196ba89ce21630ef6115d4f71f52339b5ff77
That_Guy977 <sirawit.andy977@gmail.com>
2021-10-21 21:12:26 +0700

    Update botPresence.js
    Fixed timestamps, removed unnecessary function

--- src/jphelp/events/botPresence.js
+++ src/jphelp/events/botPresence.js
@@ -6,34 +6,28 @@ const { thisFile } = getSource(import.meta.url)
 
 export const event = new Event('presenceUpdate', async (client, oldPresence, presence) => {
   if (presence.guild.id !== client.guild.id) return
-  if (client.data.alternate && !client.state.active) return
   if (isOnline(presence) === isOnline(oldPresence)) return
   const [, defaultColor, logUsers] = evtData[thisFile][client.data.guild]
   if (!logUsers.some((id) => client.resolveId(id, 'users') === presence.userId)) return
   if (presence.user.partial) await presence.user.fetch()
   const color = defaultColor ?? client.getColor(presence.userId)
+  const embed = new MessageEmbed()
+  .setTitle(`${presence.user.username} ${isOnline(presence) ? "online!" : "offline."}`)
+  .setColor(color)
+  .setAuthor(presence.user.tag, presence.user.displayAvatarURL())
+  .setTimestamp()
   if (isOnline(presence)) {
     if (!client.state.offline.includes(presence.userId)) return
-    sendStatus(client, presence, color)
     client.state.offline = client.state.offline.filter((id) => id !== presence.userId)
+    client.channel.send({ embeds: [embed] })
   } else {
     setTimeout(async () => {
       if (await presence.member.fetch().then((m) => isOnline(m.presence))) return
-      sendStatus(client, presence, color, true)
+      client.channel.send({ embeds: [embed] })
       client.state.offline.push(presence.userId)
     }, 300000)
   }
 })
-
-function sendStatus(client, presence, color, offline = false) {
-  client.channel.send({ embeds: [
-    new MessageEmbed()
-    .setTitle(`${presence.user.username} ${isOnline(presence) && !offline ? "online!" : "offline."}`)
-    .setColor(color)
-    .setAuthor(presence.user.tag, presence.user.displayAvatarURL())
-    .setTimestamp()
-  ] })
-}
 
 function isOnline(presence) {
   return (presence?.status ?? 'offline') !== 'offline'
