b289492b10c31bd0123690bfa39284a6d804ba5f
That_Guy977 <sirawit.andy977@gmail.com>
2021-10-24 01:42:39 +0700

    Added handling slash commands prior to restart

--- src/jphelp/events/handleSlash.js
+++ src/jphelp/events/handleSlash.js
@@ -3,6 +3,7 @@ import { botData } from '../../shared/config.js'
 
 export const event = new Event('interactionCreate', (client, cmd) => {
   if (!cmd.isCommand()) return
+  if (!client.slash.has(cmd.commandName)) return cmd.reply({ content: "This command is not yet available.", ephemeral: true })
   const { info, run } = client.slash.get(cmd.commandName)
   if (cmd.user.id === botData.ids.users['main']) return run(client, cmd)
   if (info.test) return cmd.reply({ content: "This command is in testing.", ephemeral: true })
