032ededde4d176285fa5e76a0fae2eaa3a1d0a43
That_Guy977 <sirawit.andy977@gmail.com>
2021-10-25 07:02:59 +0700

    Added testing in init

--- src/omega/events/initDB.js
+++ src/omega/events/initDB.js
@@ -1,7 +1,11 @@
 import { Event } from '../../shared/structures.js'
 import mongoose from 'mongoose'
 
-export const event = new Event('ready', () => {
+export const event = new Event('ready', (client) => {
+  console.log(client.source)
+  console.log(process.env[`MONGO_${client.source}`])
   // not connecting
-  mongoose.connect(/* ??? */).then(console.log)
+  // const uri = `mongodb://MONGO_JPHELP:${process.env[`MONGO_${client.source}`]}@japanese101db.mcpc1.mongodb.net/Japanese101DB`
+  const uri = `mongodb+srv://MONGO_JPHELP:${process.env[`MONGO_${client.source}`]}@japanese101db.mcpc1.mongodb.net/Japanese101DB?retryWrites=true&w=majority`
+  mongoose.connect(uri).then(() => console.log(1))
 })
