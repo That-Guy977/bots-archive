4838b69cb3e697476db07f502269688cdaeb8db1
That_Guy977 <sirawit.andy977@gmail.com>
2021-10-25 07:59:42 +0700

    Move db init to jphelp event initData

--- src/omega/events/initDB.js
+++
@@ -1,7 +0,0 @@
-import { Event } from '../../shared/structures.js'
-import mongoose from 'mongoose'
-
-export const event = new Event('ready', () => {
-  const uri = `mongodb+srv://japanese101db.mcpc1.mongodb.net`
-  mongoose.connect(uri, { auth: { username: `MONGO_JPHELP`, password: process.env[`MONGO_JPHELP`] }, dbName: "Japanese101DB" }).then(console.log)
-})
