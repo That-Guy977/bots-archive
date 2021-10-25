4b5516030cd4317f8b0acbdb83668c15451890c9
That_Guy977 <sirawit.andy977@gmail.com>
2021-10-25 14:11:21 +0700

    Update linktotop.js
    Add permissions

--- src/jphelp/slash/linktotop.js
+++ src/jphelp/slash/linktotop.js
@@ -1,9 +1,21 @@
-import { Slash } from '../../shared/structures.js'
+import { Client, Slash } from '../../shared/structures.js'
 
 export const command = new Slash({
   name: "linktotop",
   desc: "Provides a message linking to the start of the channel.",
   isGlobal: false,
   enabled: false,
+  permissions: [
+    {
+      id: Client.resolveId("mod", "roles", "jp101"),
+      type: "ROLE",
+      allow: true
+    },
+    {
+      id: Client.resolveId("contributor", "roles", "jp101"),
+      type: "ROLE",
+      allow: true
+    }
+  ],
   test: true
 })
