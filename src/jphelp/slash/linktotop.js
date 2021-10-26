8684645fdbe05da4c1afeebf48b6b6ab900c234f
That_Guy977 <sirawit.andy977@gmail.com>
2021-10-26 17:00:05 +0700

    Update linktotop.js
    Add response message, database data, removing old message
    Pending message collection

--- src/jphelp/slash/linktotop.js
+++ src/jphelp/slash/linktotop.js
@@ -18,7 +18,7 @@ export const command = new Slash({
       allow: true
     }
   ],
-  test: true
+  test: true //-- remove after testing
 }, async (client, cmd) => {
   const exemptChannels = cmdData['nc-manage-exempt']
   if (cmd.channel.parentId !== client.resolveId("nihongo-centre", "channels") || exemptChannels.some((id) => cmd.channelId === client.resolveId(id, "channels")))
@@ -27,21 +27,29 @@ export const command = new Slash({
   const msgLink = client.mongoose.models["nc_msglink"]
   const doc = await msgLink.findById(cmd.channelId).exec() ?? await msgLink.create({ _id: cmd.channelId })
   if (!doc.firstMsg || !await cmd.channel.messages.fetch(doc.firstMsg).catch(() => null)) {
-    let firstMsg
+    doc.firstMsg = null
     for (let i = 0; i < 5; i++) {
-      const msgs = await cmd.channel.messages.fetch({ limit: 100, before: firstMsg })
-      firstMsg = msgs.lastKey()
+      const msgs = await cmd.channel.messages.fetch({ limit: 100, before: doc.firstMsg })
+      doc.firstMsg = msgs.lastKey()
       if (msgs.size < 100) break
       if (i === 4) return recieveFirstMsg(cmd)
     }
-    await msgLink.findByIdAndUpdate(cmd.channelId, { $set: { firstMsg } }).exec()
+    doc.save()
   }
-  cmd.reply({ content: "Logged", ephemeral: true })
+  sendLink(cmd, doc)
 })
 
-function recieveFirstMsg(cmd) {
-  cmd.reply({ content: "More than 500 messages found, please provide a message ID manually.", ephemeral: true })
-  // add collector logic for message id
+function sendLink(cmd, doc) {
+  if (doc.linkMsg) cmd.channel.messages.delete(doc.linkMsg).catch(() => null)
+  cmd.reply({ content: `You may click [here](https://discord.com/channels/${cmd.guildId}/${cmd.channelId}/${doc.firstMsg}) to go to the top without scrolling.`, fetchReply: true }).then((reply) => {
+    doc.linkMsg = reply.id
+    doc.user = cmd.user.id
+    doc.save()
+  })
 }
 
-// add function to remove old message, send new message, overwrite database (user, linkMsg)
+function recieveFirstMsg(cmd, doc) {
+  cmd.reply({ content: "More than 500 messages found, please provide a message ID manually.", ephemeral: true })
+  doc.firstMsg = null
+  // add collector logic for message id
+}
