4d5fd7bc7f936c60400f4284b571a66b652b3a72
That_Guy977 <sirawit.andy977@gmail.com>
2021-10-27 08:37:49 +0700

    Finalized linktotop.js
    Removed message collector in favor of optional option

--- src/jphelp/slash/linktotop.js
+++ src/jphelp/slash/linktotop.js
@@ -1,4 +1,5 @@
 import { Client, Slash } from '../../shared/structures.js'
+import { isSnowflake } from '../../shared/util.js'
 import { cmdData } from '../../shared/config.js'
 
 export const command = new Slash({
@@ -6,6 +7,12 @@ export const command = new Slash({
   desc: "Provides a message linking to the start of the channel.",
   isGlobal: false,
   enabled: false,
+  options: [{
+    name: "id",
+    desc: "Message ID to link to.",
+    type: "STRING",
+    required: false
+  }],
   permissions: [
     {
       id: Client.resolveId("mod", "roles", "jp101"),
@@ -17,26 +24,36 @@ export const command = new Slash({
       type: "ROLE",
       allow: true
     }
-  ],
-  test: true //-- remove after testing
+  ]
 }, async (client, cmd) => {
   const exemptChannels = cmdData['nc-manage-exempt']
   if (cmd.channel.parentId !== client.resolveId("nihongo-centre", "channels") || exemptChannels.some((id) => cmd.channelId === client.resolveId(id, "channels")))
-    // return cmd.reply({ content: "This command is not available in this channel.", ephemeral: true })
-    ; //-- revert to filter after testing
+    return cmd.reply({ content: "This command is not available in this channel.", ephemeral: true })
   const msgLink = client.mongoose.models["nc_msglink"]
   const doc = await msgLink.findById(cmd.channelId).exec() ?? await msgLink.create({ _id: cmd.channelId })
-  if (!doc.firstMsg || !await cmd.channel.messages.fetch(doc.firstMsg).catch(() => null)) {
-    doc.firstMsg = null
-    for (let i = 0; i < 5; i++) {
-      const msgs = await cmd.channel.messages.fetch({ limit: 100, before: doc.firstMsg })
-      doc.firstMsg = msgs.lastKey()
-      if (msgs.size < 100) break
-      if (i === 4) return recieveFirstMsg(cmd, doc)
+  if (!cmd.options.get("id")) {
+    if (!doc.firstMsg || !await cmd.channel.messages.fetch(doc.firstMsg).catch(() => null)) {
+      doc.firstMsg = null
+      for (let i = 0; i < 5; i++) {
+        const msgs = await cmd.channel.messages.fetch({ limit: 100, before: doc.firstMsg })
+        doc.firstMsg = msgs.lastKey()
+        if (msgs.size < 100) break
+        if (i === 4) {
+          doc.firstMsg = null
+          doc.save()
+          return cmd.reply({ content: "More than 500 messages found, please provide a message ID manually with `/linktotop id:MSG_ID`.", ephemeral: true })
+        }
+      }
+      doc.save()
     }
-    doc.save()
+    sendLink(cmd, doc)
+  } else {
+    const id = cmd.options.get("id").value
+    if (!isSnowflake(id)) return cmd.reply({ content: `\`${id}\` is not a valid ID.`, ephemeral: true })
+    if (!await cmd.channel.messages.fetch(id).catch(() => null)) return cmd.reply({ content: `Message of ID \`${id}\` not found.`, ephemeral: true })
+    doc.firstMsg = id
+    sendLink(cmd, doc)
   }
-  sendLink(cmd, doc)
 })
 
 function sendLink(cmd, doc) {
@@ -44,28 +61,6 @@ function sendLink(cmd, doc) {
   cmd.reply({ content: `You may click [here](https://discord.com/channels/${cmd.guildId}/${cmd.channelId}/${doc.firstMsg}) to go to the top without scrolling.`, fetchReply: true }).then((reply) => {
     doc.linkMsg = reply.id
     doc.user = cmd.user.id
-    doc.save()
-  })
-}
-
-function recieveFirstMsg(cmd, doc) {
-  cmd.reply({ content: "More than 500 messages found, please provide a message ID manually.", ephemeral: true })
-  doc.firstMsg = null
-  // add collector logic for message id
-  cmd.channel.awaitMessages({
-    // update filter
-    filter: (msg) => msg.author.id === cmd.user.id,
-    max: 1,
-    // change time to 30000 after testing
-    time: 5000,
-    errors: ['time']
-  }).then((coll) => {
-    // add recieving logic
-    const msg = coll.first()
-  }).catch(() => {
-    // figure out how to check for the ephemeral message getting deleted
-    // might followUp instead if deleted
-    cmd.editReply("More than 500 messages found, please provide a message ID manually.\nNo valid message ID recieved, cancelling command.")
     doc.save()
   })
 }
