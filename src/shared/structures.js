8717045d778e2a41c2bccb2656e072421186d08a
That_Guy977 <sirawit.andy977@gmail.com>
2021-10-27 16:56:52 +0700

    Update structures.js
    Shuffled structure order around

--- src/shared/structures.js
+++ src/shared/structures.js
@@ -1,4 +1,4 @@
 export { default as Client } from '../structures/Client.js'
-export { default as Command } from '../structures/Command.js'
-export { default as Event } from '../structures/Event.js'
 export { default as ApplicationCommand } from '../structures/ApplicationCommand.js'
+export { default as Event } from '../structures/Event.js'
+export { default as Command } from '../structures/Command.js'
