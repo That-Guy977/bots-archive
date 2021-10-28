83db8f9b0c68cd17745501886dff2a1dfe141391
That_Guy977 <sirawit.andy977@gmail.com>
2021-10-28 17:00:09 +0700

    Added nc_msglink channel name updates
    Added logic to update nc_msglink channel name in channelUpdate

--- src/jphelp/events/nc_chnEdit.js
+++ src/jphelp/events/nc_chnEdit.js
@@ -5,8 +5,14 @@ export const event = new Event('channelUpdate', async (client, _oldChannel, chan
   if (channel.type !== 'GUILD_TEXT') return
   if (channel.parentId !== client.resolveId('nihongo-centre', 'channels')) return
   const archive = client.mongoose.models['nc_message']
+  const msgLink = client.mongoose.models['nc_msglink']
   const doc = await archive.findById(channel.id).exec() ?? await archive.create({ _id: channel.id, name: channel.name, createdTimestamp: channel.createdTimestamp })
+  const linkDoc = await msgLink.findById(channel.id).exec()
   if (doc.name !== channel.name) doc.name = channel.name
+  if (linkDoc && linkDoc?.name !== channel.name) {
+    linkDoc.name = channel.name
+    linkDoc.save()
+  }
   //-- add fetching messages from last saved
   doc.save()
 })
