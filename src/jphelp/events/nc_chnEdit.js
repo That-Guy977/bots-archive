42da2af23ecf9001ed0fcfbc97c18016b11aeffd
That_Guy977 <sirawit.andy977@gmail.com>
2021-10-28 14:09:21 +0700

    Add nc_chnEdit event [incomplete]
    Added channelUpdate event to update or create database entries, pending fetching messages

--- src/jphelp/events/nc_chnEdit.js
+++ src/jphelp/events/nc_chnEdit.js
@@ -1,3 +1,12 @@
 import { Event } from '../../shared/structures.js'
 
-export const event = new Event('channelUpdate', () => null)
+export const event = new Event('channelUpdate', async (client, oldChannel, channel) => {
+  if (channel.guildId !== client.guild.id) return
+  if (channel.type !== 'GUILD_TEXT') return
+  if (channel.parentId !== client.resolveId('nihongo-centre', 'channels')) return
+  const archive = client.mongoose.models['nc_message']
+  const doc = await archive.findById(channel.id).exec() ?? await archive.create({ _id: channel.id, name: channel.name, createdTimestamp: channel.createdTimestamp })
+  if (doc.name !== channel.name) doc.name = channel.name
+  //-- add fetching messages from last saved
+  doc.save()
+})
