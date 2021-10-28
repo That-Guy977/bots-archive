10364e3c0913a091429d2b7b4bdb306cadde5c09
That_Guy977 <sirawit.andy977@gmail.com>
2021-10-28 17:36:53 +0700

    Update nc_chnEdit and nc_chnDel
    Added nc-manage-exempt channel checks

--- src/jphelp/events/nc_chnEdit.js
+++ src/jphelp/events/nc_chnEdit.js
@@ -1,9 +1,11 @@
 import { Event } from '../../shared/structures.js'
+import { cmdData } from '../../shared/config.js'
 
 export const event = new Event('channelUpdate', async (client, _oldChannel, channel) => {
   if (channel.guildId !== client.guild.id) return
   if (channel.type !== 'GUILD_TEXT') return
   if (channel.parentId !== client.resolveId('nihongo-centre', 'channels')) return
+  if (cmdData['nc-manage-exempt'].some((id) => client.resolveId(id, 'channels') === channel.id)) return
   const archive = client.mongoose.models['nc_message']
   const msgLink = client.mongoose.models['nc_msglink']
   const doc = await archive.findById(channel.id).exec() ?? await archive.create({ _id: channel.id, name: channel.name, createdTimestamp: channel.createdTimestamp })
