18ff9a0f680650d038c3938d019a693a112c4553
That_Guy977 <sirawit.andy977@gmail.com>
2021-10-28 17:36:10 +0700

    Add nc_chnDel event
    Added channelDelete event to update database entries for deleted state

--- src/jphelp/events/nc_chnDel.js
+++ src/jphelp/events/nc_chnDel.js
@@ -1,3 +1,17 @@
 import { Event } from '../../shared/structures.js'
 
-export const event = new Event('channelDelete', () => null)
+export const event = new Event('channelDelete', async (client, channel) => {
+  if (channel.guildId !== client.guild.id) return
+  if (channel.type !== 'GUILD_TEXT') return
+  if (channel.parentId !== client.resolveId('nihongo-centre', 'channels')) return
+  const archive = client.mongoose.models['nc_message']
+  const doc = await archive.findById(channel.id).exec()
+  doc.deleted = true
+  doc.deletedTimestamp = Date.now()
+  doc.messages.forEach((message) => {
+    if (message.deleted) return
+    message.deleted = true
+    message.deletedTimestamp = Date.now()
+  })
+  doc.save()
+})
