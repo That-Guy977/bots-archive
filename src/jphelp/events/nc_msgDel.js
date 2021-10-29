6781255cc45f436fe9662a4e59d6b2544f9904b3
That_Guy977 <sirawit.andy977@gmail.com>
2021-10-29 07:28:47 +0700

    Added nc_msgDel event
    Added messageDelete event to update database entry

--- src/jphelp/events/nc_msgDel.js
+++ src/jphelp/events/nc_msgDel.js
@@ -1,3 +1,13 @@
 import { Event } from '../../shared/structures.js'
+import { cmdData } from '../../shared/config.js'
 
-export const event = new Event('messageDelete', () => null)
+export const event = new Event('messageDelete', async (client, message) => {
+  if (message.channel.parentId !== client.resolveId('nihongo-centre', 'channels')) return
+  if (cmdData['nc-manage-exempt'].some((id) => client.resolveId(id, 'channels') === message.channelId)) return
+  const archive = client.mongoose.models['nc_message']
+  const doc = await archive.findById(message.channelId).exec()
+  const msgDoc = doc.messages.id(message.id)
+  msgDoc.deleted = true
+  msgDoc.deletedTimestamp = Date.now()
+  doc.save()
+})
