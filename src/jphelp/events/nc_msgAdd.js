78d56eef8901436328f91e70fa55ad76d2a7d319
That_Guy977 <sirawit.andy977@gmail.com>
2021-10-29 14:11:56 +0700

    Add nc_msgAdd event
    Add messageCreate event to create database entries

--- src/jphelp/events/nc_msgAdd.js
+++ src/jphelp/events/nc_msgAdd.js
@@ -1,3 +1,23 @@
 import { Event } from '../../shared/structures.js'
+import { cmdData } from '../../shared/config.js'
+import fetch from 'node-fetch'
 
-export const event = new Event('messageCreate', () => null)
+export const event = new Event('messageCreate', async (client, message) => {
+  if (message.channel.parentId !== client.resolveId('nihongo-centre', 'channels')) return
+  if (cmdData['nc-manage-exempt'].some((id) => client.resolveId(id, 'channels') === message.channelId)) return
+  const archive = client.mongoose.models['nc_message']
+  const doc = await archive.findById(message.channelId).exec()
+  doc.messages.push({
+    _id: message.id,
+    content: message.content,
+    attachments: await Promise.all(message.attachments.map(async (attachment) => ({
+      _id: attachment.id,
+      file: await fetch(attachment.url).then((res) => res.buffer()),
+      name: attachment.name,
+      url: attachment.url
+    }))),
+    author: message.author.id,
+    createdTimestamp: message.createdTimestamp
+  })
+  doc.save()
+})
