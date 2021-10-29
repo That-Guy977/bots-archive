1d082ae6b5c5e53d8ecdd5a0eb5532513ca408ba
That_Guy977 <sirawit.andy977@gmail.com>
2021-10-29 08:18:01 +0700

    Update nc_msgBulkDel
    removed for..of loop in favor of forEach

--- src/jphelp/events/nc_msgBulkDel.js
+++ src/jphelp/events/nc_msgBulkDel.js
@@ -1,18 +1,16 @@
 import { Event } from '../../shared/structures.js'
 import { cmdData } from '../../shared/config.js'
 
-/* eslint-disable no-continue */
-
 export const event = new Event('messageDeleteBulk', async (client, messages) => {
   if (messages.first().channel.parentId !== client.resolveId('nihongo-centre', 'channels')) return
   if (cmdData['nc-manage-exempt'].some((id) => client.resolveId(id, 'channels') === messages.first().channelId)) return
   const archive = client.mongoose.models['nc_message']
   const doc = await archive.findById(messages.first().channelId).exec()
-  for (const [, message] of messages) {
+  messages.forEach((message) => {
     const msgDoc = doc.messages.id(message.id)
-    if (!msgDoc) continue
+    if (!msgDoc) return
     msgDoc.deleted = true
     msgDoc.deletedTimestamp = Date.now()
-  }
+  })
   doc.save()
 })
