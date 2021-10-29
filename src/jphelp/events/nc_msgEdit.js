15ef325c703159cf4fd6c2bcfc33b166d10a4901
That_Guy977 <sirawit.andy977@gmail.com>
2021-10-29 15:03:13 +0700

    Add nc_msgEdit event
    Add messageUpdate event to update database entries

--- src/jphelp/events/nc_msgEdit.js
+++ src/jphelp/events/nc_msgEdit.js
@@ -1,3 +1,23 @@
 import { Event } from '../../shared/structures.js'
+import { cmdData } from '../../shared/config.js'
+import fetch from 'node-fetch'
 
-export const event = new Event('messageUpdate', () => null)
+export const event = new Event('messageUpdate', async (client, _oldMessage, message) => {
+  if (message.channel.parentId !== client.resolveId('nihongo-centre', 'channels')) return
+  if (cmdData['nc-manage-exempt'].some((id) => client.resolveId(id, 'channels') === message.channelId)) return
+  const archive = client.mongoose.models['nc_message']
+  const doc = await archive.findById(message.channelId).exec()
+  const msgDoc = await doc.messages.id(message.id)
+  msgDoc.content = message.content
+  await Promise.all(
+    message.attachments
+    .filter((attachment) => !msgDoc.attachments.map((att) => att._id).includes(attachment.id))
+    .map(async (attachment) => msgDoc.attachments.push({
+      _id: attachment.id,
+      file: await fetch(attachment.url).then((res) => res.buffer()),
+      name: attachment.name,
+      url: attachment.url
+    }))
+  )
+  doc.save()
+})
