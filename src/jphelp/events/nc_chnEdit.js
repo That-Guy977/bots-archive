163900b11c00c3e78606662657406b3c992d4381
That_Guy977 <sirawit.andy977@gmail.com>
2021-10-31 03:32:39 +0700

    Update nc_chnEdit
    Move updating msgLink to function
    Add new message fetching

--- src/jphelp/events/nc_chnEdit.js
+++ src/jphelp/events/nc_chnEdit.js
@@ -1,20 +1,48 @@
 import { Event } from '../../shared/structures.js'
 import { cmdData } from '../../shared/config.js'
+import fetch from 'node-fetch'
 
 export const event = new Event('channelUpdate', async (client, _oldChannel, channel) => {
   if (channel.guildId !== client.guild.id) return
   if (channel.type !== 'GUILD_TEXT') return
   if (channel.parentId !== client.resolveId('nihongo-centre', 'channels')) return
   if (cmdData['nc-manage-exempt'].some((id) => client.resolveId(id, 'channels') === channel.id)) return
+  updateMsgLink(client, channel)
   const archive = client.mongoose.models['nc_message']
-  const msgLink = client.mongoose.models['nc_msglink']
   const doc = await archive.findById(channel.id).exec() ?? await archive.create({ _id: channel.id, name: channel.name, createdTimestamp: channel.createdTimestamp })
-  const linkDoc = await msgLink.findById(channel.id).exec()
   if (doc.name !== channel.name) doc.name = channel.name
-  if (linkDoc && linkDoc?.name !== channel.name) {
-    linkDoc.name = channel.name
-    linkDoc.save()
+  else {
+    const messageColls = []
+    let lastMsg = doc.messages[0]?._id ?? "0"
+    while (!(messageColls[0]?.size < 10)) {
+      messageColls.unshift(await channel.messages.fetch({ limit: 10, after: lastMsg }))
+      lastMsg = messageColls[0].firstKey()
+    }
+    const messages = messageColls.map((coll) => [...coll.values()]).flat().reverse()
+    for (const message of messages) {
+      doc.messages.unshift({
+        _id: message.id,
+        content: message.content,
+        attachments: await Promise.all(message.attachments.map(async (attachment) => ({
+          _id: attachment.id,
+          file: await fetch(attachment.url).then((res) => res.buffer()),
+          name: attachment.name,
+          url: attachment.url
+        }))),
+        author: message.author.id,
+        createdTimestamp: message.createdTimestamp
+      })
+    }
   }
-  //-- add fetching messages from last saved
   doc.save()
 })
+
+async function updateMsgLink(client, channel) {
+  const msgLink = client.mongoose.models['nc_msglink']
+  const doc = await msgLink.findById(channel.id).exec()
+  if (!doc) return
+  if (doc.name !== channel.name) {
+    doc.name = channel.name
+    doc.save()
+  }
+}
