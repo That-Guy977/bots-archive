19b7c2db635b3227112d3c706cd33ac4910e3566
That_Guy977 <sirawit.andy977@gmail.com>
2021-11-11 09:34:00 +0700

    Update linktotop
    fixed interactions with nc_msgDel causing database to get wiped

--- src/jphelp/interactionCommands/linktotop.js
+++ src/jphelp/interactionCommands/linktotop.js
@@ -19,17 +19,18 @@ export const command = new ApplicationCommand({
     }
   ]
 }, async (client, cmd) => {
-  if (!chnArchived(cmd.channel) && cmd.channelId !== "907295100361654273") return cmd.reply({ content: "This command is not available in this channel.", ephemeral: true })
+  if (!chnArchived(cmd.channel)) return cmd.reply({ content: "This command is not available in this channel.", ephemeral: true })
   const msgLink = client.mongoose.models['nc_msglink']
   const doc = await msgLink.findById(cmd.channelId).exec() ?? await msgLink.create({ _id: cmd.channelId, name: cmd.channel.name })
+  const prevMsg = doc.linkMsg
   doc.firstMsg = await cmd.channel.messages.fetch({ limit: 1, after: "0" }).then((coll) => coll.firstKey())
   if (!doc.firstMsg) return cmd.reply({ content: "This channel doesn't have any messages!", ephemeral: true })
-  cmd.channel.messages.delete(doc.linkMsg).catch(() => null)
   const reply = await cmd.reply({
     content: `Check the pinned messages to view chapters, or click [here](https://discord.com/channels/${cmd.guildId}/${cmd.channelId}/${doc.firstMsg}) to jump to the top.`,
     fetchReply: true
   })
   doc.linkMsg = reply.id
   doc.user = cmd.user.id
-  doc.save()
+  await doc.save()
+  cmd.channel.messages.delete(prevMsg).catch(() => null)
 })
