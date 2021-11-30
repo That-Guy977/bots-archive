3690fa024765f73569f455d5accf1948276f55fd
That_Guy977 <sirawit.andy977@gmail.com>
2021-11-30 16:33:31 +0700

    Remove rateLimit event

--- src/shared/events/rateLimit.js
+++
@@ -1,7 +0,0 @@
-import { Event } from '../../shared/structures.js'
-
-export const event = new Event('rateLimit', (client, data) => {
-  const str = `Rate limit hit at \`${data.method.toUpperCase()} ${data.path}\`\nLimited to \`${data.limit}\` requests. Rate limit ends in \`${data.timeout} ms\`.`
-  console.log(str.replace(/`/g, ''))
-  client.channel.send(str).catch(() => console.log(`Failed to send rate limit data to #${client.channel.name}.`))
-})
