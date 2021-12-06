79d34a52c0a1f1b332f7f921c5606211a00af9b1
That_Guy977 <sirawit.andy977@gmail.com>
2021-12-06 18:28:34 +0700

    Update handleCommand
    Change typeguard used

--- src/shared/events/handleCommand.js
+++ src/shared/events/handleCommand.js
@@ -3,7 +3,7 @@ import { readFile } from 'node:fs/promises'
 const { ids } = JSON.parse(await readFile('../shared/data.json'))
 
 export default new Event('interactionCreate', (client, cmd) => {
-  if (!cmd.isCommand() && !cmd.isContextMenu()) return
+  if (!cmd.isApplicationCommand()) return
   if (!client.commands.has(cmd.commandName)) return cmd.reply({ content: "This command is not yet available.", ephemeral: true })
   const { info, run } = client.commands.get(cmd.commandName)
   if (cmd.user.id === ids.users['main']) return run(client, cmd)
