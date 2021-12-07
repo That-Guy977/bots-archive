c89be966be55dca7e23996a8ed8bdeda588eba5e
That_Guy977 <sirawit.andy977@gmail.com>
2021-12-07 09:12:01 +0700

    Update handleCommand
    Remove test checks

--- src/shared/events/handleCommand.js
+++ src/shared/events/handleCommand.js
@@ -1,12 +1,7 @@
 import { Event } from '../../shared/structures.js'
-import { readFile } from 'node:fs/promises'
-const { ids } = JSON.parse(await readFile('../shared/data.json'))
 
 export default new Event('interactionCreate', (client, cmd) => {
   if (!cmd.isApplicationCommand()) return
   if (!client.commands.has(cmd.commandName)) return cmd.reply({ content: "This command is not yet available.", ephemeral: true })
-  const { info, run } = client.commands.get(cmd.commandName)
-  if (cmd.user.id === ids.users['main']) return run(client, cmd)
-  if (info.test) return cmd.reply({ content: "This command is in testing.", ephemeral: true })
-  run(client, cmd)
+  client.commands.get(cmd.commandName).run(client, cmd)
 })
