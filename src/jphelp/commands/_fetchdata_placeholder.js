0ee83fcc5ba8568a83c5a8a6183d1023fb79f7d9
That_Guy977 <sirawit.andy977@gmail.com>
2021-12-15 02:32:27 +0700

    Add placeholder fetchdata command

---
+++ src/jphelp/commands/_fetchdata_placeholder.js
@@ -0,0 +1,8 @@
+import { Command } from '../../shared/structures.js'
+
+export default new Command({
+  name: "fetchdata",
+  description: "This command has been deactivated as development has been suspended.",
+  isGlobal: false,
+  isEnabled: false
+}, () => null)
