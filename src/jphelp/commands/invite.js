1035b987f7255a270fc1a0a63dce533263745168
That_Guy977 <sirawit.andy977@gmail.com>
2022-03-06 17:11:05 +0700

    Update commands/invite
    Set vanity invite when available

--- src/jphelp/commands/invite.js
+++ src/jphelp/commands/invite.js
@@ -1,15 +1,20 @@
 import { Command } from '../../shared/structures.js'
+import { setVanity } from '../../shared/util.js'
+import { readFile } from 'node:fs/promises'
+const { config } = JSON.parse(await readFile('shared/data.json'))
 
 export default new Command({
   name: "invite",
   description: "Gives the invite link of the server.",
   isGlobal: false
-}, (client, cmd) => {
+}, async (client, cmd) => {
+  const { guild } = cmd
+  if (!guild.vanityURLCode && guild.premiumTier === 'TIER_3') await setVanity(client, guild)
   cmd.reply(
     `**Invite link of ${
-      cmd.guild.name
+      guild.name
     }**: https://discord.gg/7hvYKa4Zek\nVanity invite: ${
-      cmd.guild.vanityURLCode ?? "Unavailable"
+      guild.vanityURLCode ?? (guild.premiumTier === 'TIER_3' ? config.vanity[client.source] : "Unavailable")
     }\nThe invite link is also available in ${
       client.getChannel('rules')
     }.`
