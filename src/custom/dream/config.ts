aeb2883f92e0959ce5450c9ae6b825b11c42396e
That_Guy977 <sirawit.andy977@gmail.com>
2022-10-06 00:55:31 +0700

    Add custom config: dream, prccr

---
+++ src/custom/dream/config.ts
@@ -0,0 +1,9 @@
+import type { ClientConfig, ClientOptions, IdConfig } from "@/types";
+
+export const config: ClientConfig = {};
+
+export const clientOptions: ClientOptions = {};
+
+export const idConfig: IdConfig = {
+  guild: "779150625433976864",
+};
---
+++ src/custom/prccr/config.ts
@@ -0,0 +1,9 @@
+import type { ClientConfig, ClientOptions, IdConfig } from "@/types";
+
+export const config: ClientConfig = {};
+
+export const clientOptions: ClientOptions = {};
+
+export const idConfig: IdConfig = {
+  guild: "976680311175905361",
+};
