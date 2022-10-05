7c8d9c1192d745870b3f37953114711680b3ae71
That_Guy977 <sirawit.andy977@gmail.com>
2022-10-06 00:55:13 +0700

    Update scripts, index
    Add debug mode coverage
    Add activation coverage
    Update start, debug scripts
    Add build:start, build:debug scripts

--- src/core/index.ts
+++ src/core/index.ts
@@ -4,14 +4,28 @@ import fs from "node:fs/promises";
 import "dotenv/config";
 
 const args = process.argv.slice(2);
+const debug = args[0] === "--debug";
+if (debug) args.shift();
+const run = args[0] === "--run" || !args.length;
 const sources = await fs.readdir("src/custom");
-if (args.length) {
+if (run) {
+  args.shift();
+  if (args.length) {
+    const exists = await Promise.all(args.map(sourceValid));
+    if (exists.every((e) => e))
+      for (const source of args)
+        init(source);
+    else throw new Error(`Source(s) ${args.filter((_source, i) => !exists[i]).join(", ")} not found.`);
+  } else for (const source of sources)
+    if (await sourceValid(source))
+      init(source);
+} else {
   const source = args.shift()!;
-  const debug = args[0] === "debug";
-  if (debug) args.shift();
-  if (sources.includes(source) && await fileExists(`build/custom/${source}/config.js`))
+  if (await sourceValid(source))
     init(source, args, debug);
   else throw new Error(`Source ${source} not found.`);
-} else for (const source of sources)
-  if (await fileExists(`build/custom/${source}/config.js`))
-    init(source);
+}
+
+async function sourceValid(source: string): Promise<boolean> {
+  return sources.includes(source) && await fileExists(`build/custom/${source}/config.js`);
+}
