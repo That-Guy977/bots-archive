33b5ff1a6383d96ecc8bc07304d750b4568c5a52
That_Guy977 <sirawit.andy977@gmail.com>
2022-10-08 20:36:22 +0700

    Update typings, linter config
    Update type aliases to interfaces
    Remove event key in ClientConfig

--- src/core/types.ts
+++ src/core/types.ts
@@ -6,27 +6,6 @@ import type {
   LocalizationMap,
 } from "discord.js";
 
-export type ClientOptions = Partial<DiscordClientOptions> & {
-  intents?: GatewayIntentBits[];
-};
-export type ClientConfig = {
-  scripts?: string[];
-  events?: Record<string, Record<string, string | number>>;
-};
-export type IdKey = Record<string, string>;
-export type IdConfig = {
-  guild: string;
-  channel?: IdKey;
-  user?: IdKey;
-  role?: IdKey;
-  emoji?: IdKey;
-};
-export type SourceConfig = {
-  config: ClientConfig;
-  clientOptions: ClientOptions;
-  idConfig: IdConfig;
-};
-
 export const log = {
   INFO: chalk.blue("INFO"),
   DEBUG: chalk.magenta("DEBUG"),
@@ -34,16 +13,36 @@ export const log = {
   ERROR: chalk.red("ERROR"),
 };
 
-export type CommandConfig = {
+export interface ClientConfig {
+  scripts?: string[];
+}
+export interface ClientOptions extends Partial<DiscordClientOptions> {
+  intents?: GatewayIntentBits[];
+}
+export type IdKey = Record<string, string>;
+export interface IdConfig {
+  guild: string;
+  channel?: IdKey;
+  user?: IdKey;
+  role?: IdKey;
+  emoji?: IdKey;
+}
+export interface SourceConfig {
+  config: ClientConfig;
+  clientOptions: ClientOptions;
+  idConfig: IdConfig;
+}
+
+export interface CommandConfig {
   permissions?: CommandPermissions;
   nameLocalizations?: LocalizationMap;
-};
-export type ChatInputCommandConfig = CommandConfig & {
+}
+export interface ChatInputCommandConfig extends CommandConfig {
   descriptionLocalizations?: LocalizationMap;
-};
-export type CommandPermissions = {
+}
+export interface CommandPermissions {
   default?: PermissionsString[];
   dm?: boolean;
-};
+}
 
-export type DefImport<T = unknown> = { default: T };
+export interface DefImport<T = unknown> { default: T }
