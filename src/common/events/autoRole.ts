348eee304b6a584a21b3dc13cb6e9bbc4d129061
That_Guy977 <sirawit.andy977@gmail.com>
2022-10-23 01:20:51 +0700

    Add common.event.autoRole

---
+++ src/common/events/autoRole.ts
@@ -0,0 +1,19 @@
+import EventListener from "@/structure/EventListener";
+
+export default new EventListener("guildMemberAdd", (client, member) => {
+  const config = client.config.autoRole;
+  if (!config) return;
+  if (member.guild.id !== client.getGuildId()) return;
+  const roleName = !member.user.bot ? config.member : config.bot;
+  const warnInvalid = (): void => client.warn(`Role ${roleName} add failed.`, "event.autoRole");
+  if (roleName) member.roles.add(client.getRole(roleName)).catch(warnInvalid);
+});
+
+declare module "@/types" {
+  interface ClientConfig {
+    autoRole?: {
+      member?: string;
+      bot?: string;
+    };
+  }
+}
