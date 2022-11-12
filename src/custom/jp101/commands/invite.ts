19fc045d24074d60f7a056f1e99a42d1e9a08da7
That_Guy977 <sirawit.andy977@gmail.com>
2022-11-13 02:07:39 +0700

    Update jp101.command.invite

--- src/custom/jp101/commands/invite.ts
+++ src/custom/jp101/commands/invite.ts
@@ -4,8 +4,12 @@ import { GuildFeature } from "discord.js";
 import chalk from "chalk";
 const vanityCodeDefault = "jp101";
 
-export default new ChatInputCommand("invite", "Gives the invite link of the server.", (client, cmd) => {
-  const guild = cmd.guild!;
+export default new ChatInputCommand({
+  name: "invite",
+  description: "Gives the invite link of the server.",
+  guild: "",
+}, (client, cmd) => {
+  const { guild } = cmd;
   const permaInvite = `**Invite link of ${guild.name}**: https://discord.gg/7hvYKa4Zek\n`;
   const vanityInvite = (code: string): string => `Vanity invite: ${code}\n`;
   const redirect = `The invite link is also available in ${client.getChannel("rules")}.`;
@@ -16,4 +20,4 @@ export default new ChatInputCommand("invite", "Gives the invite link of the serv
     setVanity(client).catch(warnNoAccess);
   }
   cmd.reply(`${permaInvite}${hasVanity ? vanityInvite(vanityCode ?? "jp101") : ""}${redirect}`);
-}, "");
+});
